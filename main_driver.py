111018ab23ccfe56844bd6a966ff6c4b79fec91d
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-07-01 23:29:08 -0400

    Added the main driver and buttons to the gui

---
+++ main_driver.py
@@ -0,0 +1,9 @@
+"""The main program"""
+
+from main_window import MainWindow
+
+from tkinter import *
+
+root = Tk()
+
+window = MainWindow(root)
--- main_window.py
+++ main_window.py
@@ -4,14 +4,20 @@ from tkinter import *
 
 class MainWindow():
 
-    def __init__(self, root, dataBase):
+    def __init__(self, root):
         """Constructor"""
         self.root = root
-        self.dataBase = dataBase
 
-        self.left_frame = Frame(root)
-        self.right_frame = Frame(root)
+        self.left_frame = Frame(self.root)
+        self.right_frame = Frame(self.root)
         
-        self.add_new_button = Button(right_frame, text='Add New')
-        self.remove_button = Button(right_frame, text='Remove')
+        self.add_new_button = Button(self.right_frame, text='Add New')
+        self.remove_button = Button(self.right_frame, text='Remove')
+
+        self.left_frame.pack()
+        self.right_frame.pack()
+        self.add_new_button.pack()
+        self.remove_button.pack()
+
+        self.root.mainloop()
     
