a9a2fe09c15be492bb1c73f7b7e2763293b68b13
afranco07 <alberto.franco86@qmail.cuny.edu>
2017-07-09 13:02:17 -0400

    Added the Text widget to the main window

--- main_window.py
+++ main_window.py
@@ -2,6 +2,7 @@
 
 from tkinter import *
 from entry_window import EntryWindow
+from left_frame import LeftFrame
 
 class MainWindow(object):
 
@@ -16,8 +17,13 @@ class MainWindow(object):
         self.add_new_button = Button(self.right_frame, text='Add New', command=self.open_entry_window)
         self.remove_button = Button(self.right_frame, text='Remove')
         self.close_button = Button(self.right_frame, text='Close', command=self.root.quit)
+        
+        self.address_list = Text(self.left_frame, width=30, height=20)
+        self.address_list.insert(INSERT, 'Test 1')
+        self.address_list.insert(END, 'Test 2')
+        self.address_list.pack()
 
-        self.left_frame.pack()
+        self.left_frame.pack(side=LEFT)
         self.right_frame.pack()
         self.add_new_button.pack()
         self.remove_button.pack()
@@ -28,3 +34,14 @@ class MainWindow(object):
     def open_entry_window(self):
         """Opens the EntryWindow object"""
         entry_window = EntryWindow()
+
+    def create_left_frame(self, root, frame):
+        """Initializes all of the left frame components"""
+        self.frame = Frame(root)
+
+        self.address_list = Listbox(self.frame)
+
+        self.address_list.insert(END, 'Test 1')
+        self.address_list.insert(END, 'Test 2')
+
+        self.address_list.pack()
